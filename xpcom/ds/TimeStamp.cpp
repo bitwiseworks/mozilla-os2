527fc2cb87f7f8c0b7ebdd166e2f5fe780351d04
Dave Yeo <daveryeo@telus.net>
2013-05-09 04:16:13 +0400

    Add dummy TimeStamp implementation to xpcom based on PR_IntervalNow.

---
+++ xpcom/ds/TimeStamp.cpp
@@ -0,0 +1,104 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/* vim:set ts=2 sw=2 sts=2 et cindent: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+// Dummy TimeStamp implementation using PR_IntervalNow(). Its possible
+// resolution is limited to 100000 ticks per second max (as set by
+// PR_INTERVAL_MAX).
+
+#include "mozilla/TimeStamp.h"
+#include "prlock.h"
+#define nsnull 0L
+
+namespace mozilla {
+
+struct TimeStampInitialization
+{
+  TimeStampInitialization() {
+    TimeStamp::Startup();
+  }
+  ~TimeStampInitialization() {
+    TimeStamp::Shutdown();
+  }
+};
+
+static TimeStampInitialization initOnce;
+
+static PRLock* gTimeStampLock = 0;
+static PRUint32 gRolloverCount;
+static PRIntervalTime gLastNow;
+
+double
+TimeDuration::ToSeconds() const
+{
+ return double(mValue)/PR_TicksPerSecond();
+}
+
+double
+TimeDuration::ToSecondsSigDigits() const
+{
+  return ToSeconds();
+}
+
+TimeDuration
+TimeDuration::FromMilliseconds(double aMilliseconds)
+{
+  static double kTicksPerMs = double(PR_TicksPerSecond()) / 1000.0;
+  return TimeDuration::FromTicks(aMilliseconds * kTicksPerMs);
+}
+
+TimeDuration
+TimeDuration::Resolution()
+{
+  // This is grossly nonrepresentative of actual system capabilities
+  // on some platforms
+  return TimeDuration::FromTicks(PRInt64(1));
+}
+
+nsresult
+TimeStamp::Startup()
+{
+  if (gTimeStampLock)
+    return NS_OK;
+
+  // TimeStamp has to use bare PRLock instead of mozilla::Mutex
+  // because TimeStamp can be used very early in startup.
+  gTimeStampLock = PR_NewLock();
+  gRolloverCount = 1;
+  gLastNow = 0;
+  return gTimeStampLock ? NS_OK : NS_ERROR_OUT_OF_MEMORY;
+}
+
+void
+TimeStamp::Shutdown()
+{
+  if (gTimeStampLock) {
+    PR_DestroyLock(gTimeStampLock);
+    gTimeStampLock = nsnull;
+  }
+}
+
+TimeStamp
+TimeStamp::Now()
+{
+  // XXX this could be considerably simpler and faster if we had
+  // 64-bit atomic operations
+  PR_Lock(gTimeStampLock);
+
+  PRIntervalTime now = PR_IntervalNow();
+  if (now < gLastNow) {
+    ++gRolloverCount;
+    // This can't happen unless you've been running for millions of years
+    NS_ASSERTION(gRolloverCount > 0, "Rollover in rollover count???");
+  }
+
+  gLastNow = now;
+  TimeStamp result((PRUint64(gRolloverCount) << 32) + now);
+
+  PR_Unlock(gTimeStampLock);
+  return result;
+}
+
+}
