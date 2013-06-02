94bbfa189e991de46ec7ead634926f21ca74d270
Dmitriy Kuminov <coding@dmik.org>
2013-06-02 05:24:52 +0400

    [OS/2] Fix build break in xpcom/reflect.

---
+++ xpcom/reflect/xptcall/src/md/os2/xptcinvoke_gcc_x86_os2.cpp
@@ -0,0 +1,8 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* Platform specific code to invoke XPCOM methods on native objects */
+
+#include "../unix/xptcinvoke_gcc_x86_unix.cpp"
