896a5469de10ba75c99cecf6060e9e6c831520a0
Dmitriy Kuminov <coding@dmik.org>
2013-12-12 03:09:28 +0400

    [OS/2] NSPR: Define int32 as PRInt32 rather than long.
    There is no reason for making it long at all while doing so breaks
    chromium serialization code (that always serializes long as 64 bit)
    by making int32 incompatible with int in this regard.

--- nsprpub/pr/include/obsolete/protypes.h
+++ nsprpub/pr/include/obsolete/protypes.h
@@ -73,7 +73,7 @@ typedef PRUint64 uint64;
  */
 
 #if !defined(XP_BEOS)
-#if !defined(_WIN32) && !defined(XP_OS2) && !defined(NTO)
+#if !defined(_WIN32) && !defined(NTO)
 typedef PRUint32 uint32;
 #else
 typedef unsigned long uint32;
@@ -110,7 +110,7 @@ typedef PRInt64 int64;
 
 #if !defined(XP_BEOS) && !defined(_PR_AIX_HAVE_BSD_INT_TYPES) \
     && !defined(HPUX)
-#if !defined(_WIN32) && !defined(XP_OS2) && !defined(NTO)
+#if !defined(_WIN32) && !defined(NTO)
 typedef PRInt32 int32;
 #else
 typedef long int32;
