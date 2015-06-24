a6369969d5e51ad6b0c6b5c9284e6c0e29efb115
Dmitriy Kuminov <coding@dmik.org>
2015-06-24 22:56:42 +0400

    [OS/2] osfile: Convert NSPR codes to what OS.File.Error expects.
    This fixes sessionstore.js file creation at startup.

--- toolkit/components/osfile/NativeOSFileInternals.cpp
+++ toolkit/components/osfile/NativeOSFileInternals.cpp
@@ -146,6 +146,96 @@ private:
 #error "We do not have platform-specific constants for this platform"
 #endif
 
+#if defined(XP_OS2)
+// On OS/2 PR_GetOSError returns a native OS/2 error but OS.File.Error expects
+// errno (as we go Posix-way there). Provide a converter.
+PRInt32 PR_GET_OS_ERROR()
+{
+  // We could use __libc_native2errno(PR_GetOSError()) under kLIBC but it is
+  // not exported, unfortunately. So we have to copy-paste it from there. It is
+  // important to get proper mappings, otherwise the "because" properties in
+  // osfile_unix_allthreads.jsm won't work correctly causing variuos side effects.
+
+  static unsigned char const errno_tab[] =
+  {
+    EINVAL, EINVAL, ENOENT, ENOENT, EMFILE,  /* 0..4 */
+    EACCES, EBADF,  EIO,    ENOMEM, EIO,     /* 5..9 */
+    EINVAL, ENOEXEC,EINVAL, EINVAL, EINVAL,  /* 10..14 */
+    ENOENT, EBUSY,  EXDEV,  ENOENT, EROFS,   /* 15..19 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 20..24 */
+    EIO,    EIO,    EIO,    ENOSPC, EIO,     /* 25..29 */
+    EIO,    EIO,    EACCES, EACCES, EIO,     /* 30..34 */
+    EIO,    EIO,    EIO,    EIO,    ENOSPC,  /* 35..39 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 40..44 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 45..49 */
+    EIO,    EIO,    EIO,    EIO,    EBUSY,   /* 50..54 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 55..59 */
+    EIO,    ENOSPC, ENOSPC, EIO,    EIO,     /* 60..64 */
+    EACCES, EIO,    EIO,    EIO,    EIO,     /* 65..69 */
+    EIO,    EIO,    EIO,    EROFS,  EIO,     /* 70..74 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 75..79 */
+    EEXIST, EIO,    ENOENT, EIO,    EIO,     /* 80..84 */
+    EIO,    EIO,    EINVAL, EIO,    EAGAIN,  /* 85..89 */
+    EIO,    EIO,    EIO,    EIO,    EIO,     /* 90..94 */
+    EINTR,  EIO,    EIO,    EIO,    EACCES,  /* 95..99 */
+    ENOMEM, EINVAL, EINVAL, ENOMEM, EINVAL,  /* 100..104 */
+    EINVAL, ENOMEM, EIO,    EACCES, EPIPE,   /* 105..109 */
+    ENOENT, E2BIG,  ENOSPC, ENOMEM, EBADF,   /* 110..114 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 115..119 */
+    EINVAL, EINVAL, EINVAL, ENOENT, EINVAL,  /* 120..124 */
+    ENOENT, ENOENT, ENOENT, ECHILD, ECHILD,  /* 125..129 */
+    EACCES, EINVAL, ESPIPE, EINVAL, EINVAL,  /* 130..134 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 135..139 */
+    EINVAL, EINVAL, EBUSY,  EINVAL, EINVAL,  /* 140..144 */
+    EINVAL, EINVAL, EINVAL, EBUSY,  EINVAL,  /* 145..149 */
+    EINVAL, EINVAL, ENOMEM, EINVAL, EINVAL,  /* 150..154 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 155..159 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EAGAIN,  /* 160..164 */
+    EINVAL, EINVAL, EACCES, EINVAL, EINVAL,  /* 165..169 */
+    EBUSY,  EINVAL, EINVAL, EINVAL, EINVAL,  /* 170..174 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 175..179 */
+    EINVAL, EINVAL, EINVAL, EINVAL, ECHILD,  /* 180..184 */
+    EINVAL, EINVAL, ENOENT, EINVAL, EINVAL,  /* 185..189 */
+    ENOEXEC,ENOEXEC,ENOEXEC,ENOEXEC,ENOEXEC, /* 190..194 */
+    ENOEXEC,ENOEXEC,ENOEXEC,ENOEXEC,ENOEXEC, /* 195..199 */
+    ENOEXEC,ENOEXEC,ENOEXEC,ENOENT, EINVAL,  /* 200..204 */
+    EINVAL, ENAMETOOLONG, EINVAL, EINVAL, EINVAL,  /* 205..209 */
+    EINVAL, EINVAL, EACCES, ENOEXEC,ENOEXEC, /* 210..214 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 215..219 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 220..224 */
+    EINVAL, EINVAL, EINVAL, ECHILD, EINVAL,  /* 225..229 */
+    EINVAL, EBUSY,  EAGAIN, ENOTCONN, EINVAL, /* 230..234 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 235..239 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 240..244 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 245..249 */
+    EACCES, EACCES, EINVAL, ENOENT, EINVAL,  /* 250..254 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 255..259 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 260..264 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 265..269 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 270..274 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 275..279 */
+    EINVAL, EINVAL, ENOTSUP, EINVAL, EEXIST,  /* 280..284 */
+    EEXIST, EINVAL, EINVAL, EINVAL, EINVAL,  /* 285..289 */
+    ENOMEM, EMFILE, EINVAL, EINVAL, EINVAL,  /* 290..294 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EINVAL,  /* 295..299 */
+    EINVAL, EBUSY,  EINVAL, ESRCH,  EINVAL,  /* 300..304 */
+    ESRCH,  EINVAL, EINVAL, EINVAL, ESRCH,   /* 305..309 */
+    EINVAL, ENOMEM, EINVAL, EINVAL, EINVAL,  /* 310..314 */
+    EINVAL, E2BIG,  ENOENT, EIO,    EIO,     /* 315..319 */
+    EINVAL, EINVAL, EINVAL, EINVAL, EAGAIN,  /* 320..324 */
+    EINVAL, EINVAL, EINVAL, EIO,    ENOENT,  /* 325..329 */
+    EACCES, EACCES, EACCES, ENOENT, ENOMEM   /* 330..334 */
+  };
+
+  PRUint32 rc = PR_GetOSError();
+  if (rc >= sizeof(errno_tab))
+    return EINVAL;
+  return errno_tab[rc];
+}
+#else
+#define PR_GET_OS_ERROR() PR_GetOSError()
+#endif
+
 ///////// Results of OS.File operations
 
 /**
@@ -651,7 +741,7 @@ public:
     file = PR_ImportFile((PROsfd)handle);
     if (!file) {
       // |file| is closed by PR_ImportFile
-      Fail(NS_LITERAL_CSTRING("ImportFile"), nullptr, PR_GetOSError());
+      Fail(NS_LITERAL_CSTRING("ImportFile"), nullptr, PR_GET_OS_ERROR());
       return NS_ERROR_FAILURE;
     }
 
@@ -660,7 +750,7 @@ public:
     NS_ConvertUTF16toUTF8 path(mPath);
     file = PR_OpenFile(path.get(), PR_RDONLY, 0);
     if (!file) {
-      Fail(NS_LITERAL_CSTRING("open"), nullptr, PR_GetOSError());
+      Fail(NS_LITERAL_CSTRING("open"), nullptr, PR_GET_OS_ERROR());
       return NS_ERROR_FAILURE;
     }
 
@@ -668,7 +758,7 @@ public:
 
     PRFileInfo64 stat;
     if (PR_GetOpenFileInfo64(file, &stat) != PR_SUCCESS) {
-      Fail(NS_LITERAL_CSTRING("stat"), nullptr, PR_GetOSError());
+      Fail(NS_LITERAL_CSTRING("stat"), nullptr, PR_GET_OS_ERROR());
       return NS_ERROR_FAILURE;
     }
 
@@ -690,7 +780,7 @@ public:
       just_read = PR_Read(file, dest_chars + total_read,
                           std::min(uint64_t(PR_INT32_MAX), bytes - total_read));
       if (just_read == -1) {
-        Fail(NS_LITERAL_CSTRING("read"), nullptr, PR_GetOSError());
+        Fail(NS_LITERAL_CSTRING("read"), nullptr, PR_GET_OS_ERROR());
         return NS_ERROR_FAILURE;
       }
       total_read += just_read;
