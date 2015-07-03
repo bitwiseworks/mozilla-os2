bc58af1f7ed39cf84ddb7cccd3ec84660e309895
Dmitriy Kuminov <coding@dmik.org>
2015-07-04 01:01:12 +0400

    [OS/2] Temporarily disable *all* OOP.
    This is due to broken IPC communication. See #105 and #106.

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -1121,8 +1121,10 @@ pref("dom.ipc.plugins.enabled.x86_64", true);
 #ifdef XP_OS2
 // Odin crashes MMPM in OOP mode, disable it for now
 pref("dom.ipc.plugins.enabled.npflos2.dll", false);
-// And IPC is generally broken there (a fix is TBD), so disable it at all for now
+// And IPC is generally broken now (see https://github.com/bitwiseworks/mozilla-os2/issues/106)
+// so disable it at all for the present time (both plugins and content)
 pref("dom.ipc.plugins.enabled", false);
+pref("dom.ipc.tabs.disabled", true);
 #else
 pref("dom.ipc.plugins.enabled", true);
 #endif
