bitwiseworks/mozilla-os2

commit 9a736140c9aa57ce42e5734da1d1a88fb0b7a981

[OS/2] Include everything from os2.h (part 2).
More generic version of 33b9c244de9da50b96d03fd2730b15d1ccde7d7b.

widget/os2/nsDeviceContextSpecOS2.cpp
@@ -8,10 +8,8 @@
 
 #include <stdlib.h>
 
+#define INCL_BASE
 #define INCL_PM
-#define INCL_DOS
-#define INCL_SPLDOSPRINT
-#define INCL_DEVDJP
 #include <os2.h>
 
 #define INCL_GRE_DEVICE

widget/os2/nsPrintOS2.cpp
@@ -7,11 +7,8 @@
 
 #include <stdlib.h>
 
+#define INCL_BASE
 #define INCL_PM
-#define INCL_SPLDOSPRINT
-#define INCL_DEV
-#define INCL_DEVDJP
-#define INCL_GRE_DEVICE
 #include <os2.h>
 
 #include "nsIServiceManager.h"

widget/os2/nsWindow.h
@@ -47,15 +47,13 @@
 #ifndef _nswindow_h
 #define _nswindow_h
 
-#include "nsBaseWidget.h"
-#include "mozilla/MouseEvents.h"
-
-#define INCL_DOS
-#define INCL_WIN
-#define INCL_NLS
-#define INCL_GPI
+#define INCL_BASE
+#define INCL_PM
 #include <os2.h>
 #include <os2im.h>
+
+#include "nsBaseWidget.h"
+#include "mozilla/MouseEvents.h"
 
 //-----------------------------------------------------------------------------
 // Items that may not be in the OS/2 Toolkit headers

commit 852ba0bbc6fd9ba616300ddfe72018092d2a6bf2

[OS/2] Fix os2.h inclusion in some places broken in 9a736140.
Unfortunately, there is no way to include everything from os2.h with a simple
set of defines.

widget/os2/nsDeviceContextSpecOS2.cpp
@@ -10,6 +10,9 @@
 
 #define INCL_BASE
 #define INCL_PM
+// too pity the above two don't include this one (looks like a bug):
+#define INCL_SPLDOSPRINT
+#define INCL_DEVDJP
 #include <os2.h>
 
 #define INCL_GRE_DEVICE

widget/os2/nsPrintOS2.cpp
@@ -9,6 +9,8 @@
 
 #define INCL_BASE
 #define INCL_PM
+// too pity the above two don't include this one (looks like a bug):
+#define INCL_SPLDOSPRINT
 #include <os2.h>
 
 #include "nsIServiceManager.h"

widget/os2/nsWindow.h
@@ -49,6 +49,8 @@
 
 #define INCL_BASE
 #define INCL_PM
+// too pity the above two don't include this one (looks like a bug):
+#define INCL_NLS
 #include <os2.h>
 #include <os2im.h>
 