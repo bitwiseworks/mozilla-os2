bitwiseworks/mozilla-os2

commit 9a736140c9aa57ce42e5734da1d1a88fb0b7a981

[OS/2] Include everything from os2.h (part 2).
More generic version of 33b9c244de9da50b96d03fd2730b15d1ccde7d7b.

media/libvpx/vp8/common/generic/systemdependent.c
@@ -25,8 +25,8 @@
 #include <windows.h>
 typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
 #elif defined(__OS2__)
-#define INCL_DOS
-#define INCL_DOSSPINLOCK
+#define INCL_BASE
+#define INCL_PM
 #include <os2.h>
 #endif
 #endif

commit 74f34ab084899a9546326c7e4af3c1491d6acf44

[OS/2] Fix os2.h inclusion in some places broken in 9a73614.
Unfortunately, there is no way to include everything from os2.h with a simple
set of defines.

media/libvpx/vp8/common/generic/systemdependent.c
@@ -27,6 +27,8 @@ typedef void (WINAPI *PGNSI)(LPSYSTEM_INFO);
 #elif defined(__OS2__)
 #define INCL_BASE
 #define INCL_PM
+// // too pity the above two don't include this one (looks like a bug):
+#define INCL_DOSSPINLOCK
 #include <os2.h>
 #endif
 #endif