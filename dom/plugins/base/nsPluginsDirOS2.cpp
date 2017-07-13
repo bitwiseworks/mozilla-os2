8c41204cdfed4c41497344c789d3cf21f1b8662b
Dmitriy Kuminov <coding@dmik.org>
2017-07-13 18:46:05 +0400

    [OS/2] Read plugin DLL properties w/o loading it for execution.
    This closes a potential security threat and makes the main Firefox process
    more proof to spontaneous crashes caused by bogus/dangerous plugins even
    when they are completely disabled and not used. Closes #227.

--- dom/plugins/base/nsPluginsDirOS2.cpp
+++ dom/plugins/base/nsPluginsDirOS2.cpp
@@ -8,6 +8,8 @@
 #define INCL_PM
 #include <os2.h>
 
+#include <libcx/exeinfo.h>
+
 #include "nsPluginsDir.h"
 #include "prlink.h"
 #include "plstr.h"
@@ -19,63 +21,43 @@
 
 /* Load a string stored as RCDATA in a resource segment */
 /* Returned string needs to be PR_Free'd by caller */
-static char *LoadRCDATAString( HMODULE hMod, ULONG resid)
+static char *LoadRCDATAString(EXEINFO einfo, ULONG resid)
 {
-   APIRET rc;
-   ULONG  ulSize = 0;
-   char  *string = 0;
+  char *string = nullptr;
+  const char *readOnlyString = nullptr;
 
-   rc = DosQueryResourceSize( hMod, RT_RCDATA, resid, &ulSize);
+  int len = exeinfo_get_resource_data(einfo, RT_RCDATA, resid, &readOnlyString);
 
-   if( rc == NO_ERROR)
-   {
-      char *readOnlyString = 0;
-      rc = DosGetResource( hMod, RT_RCDATA, resid, (void**) &readOnlyString);
+  if (len != -1)
+  {
+    /* allow for 0-termination if user hasn't got it right */
+    if (readOnlyString[len - 1] != '\0')
+      len++;
 
-      /* allow for 0-termination if user hasn't got it right */
-      if( readOnlyString[ ulSize - 1] != '\0')
-         ulSize++;
+    /* copy string & zero-terminate */
+    string = (char *)PR_Malloc(len);
+    memcpy(string, readOnlyString, len - 1);
+    string[len - 1] = '\0';
+  }
 
-      if( rc == NO_ERROR)
-      {
-         /* copy string & zero-terminate */
-         string = (char*) PR_Malloc( ulSize);
-         memcpy( string, readOnlyString, ulSize - 1);
-         string[ ulSize - 1] = '\0';
-
-         DosFreeResource( readOnlyString);
-      }
-   }
-
-   return string;
+  return string;
 }
 
 /* Load a version string stored as RCDATA in a resource segment */
 /* Returned string needs to be PR_Free'd by caller */
-static char *LoadRCDATAVersion(HMODULE hMod, ULONG resid)
+static char *LoadRCDATAVersion(EXEINFO einfo, ULONG resid)
 {
-   APIRET rc;
-   ULONG  ulSize = 0;
-   char  *string = 0;
+  char *string = nullptr;
+  const char *version = nullptr;
 
-   rc = DosQueryResourceSize(hMod, RT_RCDATA, resid, &ulSize);
+  int len = exeinfo_get_resource_data(einfo, RT_RCDATA, resid, &version);
 
-   // version info is should be 8 chars
-   if (rc == NO_ERROR && ulSize == 8)
-   {
-      char *version = nullptr;
-      rc = DosGetResource(hMod, RT_RCDATA, resid, (void**) &version);
+  // version info should be 8 chars
+  if (len == 8)
+    string = PR_smprintf("%d.%d.%d.%d",
+                         version[0], version[2], version[4], version[6]);
 
-      if (rc == NO_ERROR)
-      {
-         string = PR_smprintf("%d.%d.%d.%d\n",
-                              version[0], version[2], version[4], version[6]);
-
-         DosFreeResource(version);
-      }
-   }
-
-   return string;
+  return string;
 }
 
 static uint32_t CalculateVariantCount(char* mimeTypes)
@@ -138,23 +120,38 @@ static void FreeStringArray(uint32_t variants, char ** array)
 
 bool nsPluginsDir::IsPluginFile(nsIFile* file)
 {
-    nsAutoCString leaf;
-    if (NS_FAILED(file->GetNativeLeafName(leaf)))
-        return false;
+  nsAutoCString leaf;
+  if (NS_FAILED(file->GetNativeLeafName(leaf)))
+    return false;
 
-    const char *leafname = leaf.get();
-    
-    if( nullptr != leafname)
+  const char *leafname = leaf.get();
+
+  if( nullptr != leafname)
+  {
+    int len = strlen (leafname);
+    if( len > 6 &&                 // np*.dll
+        (0 == strnicmp (&(leafname[len - 4]), ".dll", 4)) &&
+        (0 == strnicmp (leafname, "np", 2)))
     {
-      int len = strlen( leafname);
-      if( len > 6 &&                 // np*.dll
-          (0 == strnicmp( &(leafname[len - 4]), ".dll", 4)) &&
-          (0 == strnicmp( leafname, "np", 2)))
+      nsAutoCString fname;
+      if (NS_SUCCEEDED(file->GetNativePath(fname)))
       {
-        return true;
+        EXEINFO_FORMAT fmt = EXEINFO_FORMAT_INVALID;
+        EXEINFO info = exeinfo_open(fname.get());
+        if (info)
+        {
+          fmt = exeinfo_get_format(info);
+          exeinfo_close(info);
+        }
+
+        // We only support LX plugin DLLs
+        if (fmt == EXEINFO_FORMAT_LX)
+          return true;
       }
     }
-    return false;
+  }
+
+  return false;
 }
 
 // nsPluginFile implementation
@@ -169,106 +166,113 @@ nsPluginFile::~nsPluginFile()
 // Loads the plugin into memory using NSPR's shared-library loading
 nsresult nsPluginFile::LoadPlugin(PRLibrary **outLibrary)
 {
-    if (!mPlugin)
-      return NS_ERROR_NULL_POINTER;
-   
-    nsAutoCString temp;
-    mPlugin->GetNativePath(temp);
+  if (!mPlugin)
+    return NS_ERROR_NULL_POINTER;
 
-    *outLibrary = PR_LoadLibrary(temp.get());
-    return *outLibrary == nullptr ? NS_ERROR_FAILURE : NS_OK;
+  nsAutoCString temp;
+  mPlugin->GetNativePath(temp);
+
+  *outLibrary = PR_LoadLibrary(temp.get());
+  return *outLibrary == nullptr ? NS_ERROR_FAILURE : NS_OK;
 }
 
 // Obtains all of the information currently available for this plugin.
 nsresult nsPluginFile::GetPluginInfo(nsPluginInfo &info, PRLibrary **outLibrary)
 {
-   *outLibrary = nullptr;
+  *outLibrary = nullptr;
 
-   nsresult   rv = NS_ERROR_FAILURE;
-   HMODULE    hPlug = 0; // Need a HMODULE to query resource statements
-   char       failure[ CCHMAXPATH] = "";
-   APIRET     ret;
+  nsresult   rv = NS_ERROR_FAILURE;
 
-   nsAutoCString path;
-   if (NS_FAILED(rv = mPlugin->GetNativePath(path)))
-     return rv;
+  nsAutoCString path;
+  if (NS_FAILED(rv = mPlugin->GetNativePath(path)))
+    return rv;
 
-   nsAutoCString fileName;
-   if (NS_FAILED(rv = mPlugin->GetNativeLeafName(fileName)))
-     return rv;
+  nsAutoCString fileName;
+  if (NS_FAILED(rv = mPlugin->GetNativeLeafName(fileName)))
+    return rv;
 
-   ret = DosLoadModule( failure, CCHMAXPATH, path.get(), &hPlug);
-   info.fVersion = nullptr;
+  info.fVersion = nullptr;
 
-   while( ret == NO_ERROR)
-   {
-      info.fName = LoadRCDATAString( hPlug, NP_INFO_ProductName);
+  EXEINFO einfo = exeinfo_open(path.get());
 
-      info.fVersion = LoadRCDATAVersion( hPlug, NP_INFO_ProductVersion);
-
-      // get description (doesn't matter if it's missing)...
-      info.fDescription = LoadRCDATAString( hPlug, NP_INFO_FileDescription);
-
-      char * mimeType = LoadRCDATAString( hPlug, NP_INFO_MIMEType);
-      if( nullptr == mimeType) break;
-
-      char * mimeDescription = LoadRCDATAString( hPlug, NP_INFO_FileOpenName);
-      if( nullptr == mimeDescription) break;
-
-      char * extensions = LoadRCDATAString( hPlug, NP_INFO_FileExtents);
-      if( nullptr == extensions) break;
-
-      info.fVariantCount = CalculateVariantCount(mimeType);
-
-      info.fMimeTypeArray = MakeStringArray(info.fVariantCount, mimeType);
-      if( info.fMimeTypeArray == nullptr) break;
-
-      info.fMimeDescriptionArray = MakeStringArray(info.fVariantCount, mimeDescription);
-      if( nullptr == info.fMimeDescriptionArray) break;
-
-      info.fExtensionArray = MakeStringArray(info.fVariantCount, extensions);
-      if( nullptr == info.fExtensionArray) break;
-
-      info.fFullPath = PL_strdup(path.get());
-      info.fFileName = PL_strdup(fileName.get());
-
-      rv = NS_OK;
+  do
+  {
+    if (!einfo)
       break;
-   }
 
-   if( 0 != hPlug)
-      DosFreeModule( hPlug);
+    info.fName = LoadRCDATAString(einfo, NP_INFO_ProductName);
 
-   return rv;
+    info.fVersion = LoadRCDATAVersion(einfo, NP_INFO_ProductVersion);
+
+    // get description (doesn't matter if it's missing)...
+    info.fDescription = LoadRCDATAString(einfo, NP_INFO_FileDescription);
+
+    char* mimeType = LoadRCDATAString(einfo, NP_INFO_MIMEType);
+    if (nullptr == mimeType)
+      break;
+
+    char* mimeDescription = LoadRCDATAString(einfo, NP_INFO_FileOpenName);
+    if (nullptr == mimeDescription)
+      break;
+
+    char* extensions = LoadRCDATAString(einfo, NP_INFO_FileExtents);
+    if (nullptr == extensions)
+      break;
+
+    info.fVariantCount = CalculateVariantCount(mimeType);
+
+    info.fMimeTypeArray = MakeStringArray(info.fVariantCount, mimeType);
+    if (info.fMimeTypeArray == nullptr)
+      break;
+
+    info.fMimeDescriptionArray = MakeStringArray(info.fVariantCount, mimeDescription);
+    if (nullptr == info.fMimeDescriptionArray)
+      break;
+
+    info.fExtensionArray = MakeStringArray(info.fVariantCount, extensions);
+    if (nullptr == info.fExtensionArray)
+      break;
+
+    info.fFullPath = PL_strdup(path.get());
+    info.fFileName = PL_strdup(fileName.get());
+
+    rv = NS_OK;
+  }
+  while (0);
+
+  if (einfo)
+    exeinfo_close(einfo);
+
+  return rv;
 }
 
 nsresult nsPluginFile::FreePluginInfo(nsPluginInfo& info)
 {
-   if(info.fName != nullptr)
-     PL_strfree(info.fName);
+  if (info.fName != nullptr)
+    PL_strfree(info.fName);
 
-   if(info.fFullPath != nullptr)
-     PL_strfree(info.fFullPath);
+  if (info.fFullPath != nullptr)
+    PL_strfree(info.fFullPath);
 
-   if(info.fFileName != nullptr)
-     PL_strfree(info.fFileName);
- 
-   if(info.fVersion != nullptr)
-     PL_strfree(info.fVersion);
- 
-   if(info.fDescription != nullptr)
-     PL_strfree(info.fDescription);
- 
-   if(info.fMimeTypeArray != nullptr)
-     FreeStringArray(info.fVariantCount, info.fMimeTypeArray);
- 
-   if(info.fMimeDescriptionArray != nullptr)
-     FreeStringArray(info.fVariantCount, info.fMimeDescriptionArray);
- 
-   if(info.fExtensionArray != nullptr)
-     FreeStringArray(info.fVariantCount, info.fExtensionArray);
- 
-   memset((void *)&info, 0, sizeof(info));
- 
-   return NS_OK;
+  if (info.fFileName != nullptr)
+    PL_strfree(info.fFileName);
+
+  if (info.fVersion != nullptr)
+    PL_strfree(info.fVersion);
+
+  if (info.fDescription != nullptr)
+    PL_strfree(info.fDescription);
+
+  if (info.fMimeTypeArray != nullptr)
+    FreeStringArray(info.fVariantCount, info.fMimeTypeArray);
+
+  if (info.fMimeDescriptionArray != nullptr)
+    FreeStringArray(info.fVariantCount, info.fMimeDescriptionArray);
+
+  if (info.fExtensionArray != nullptr)
+    FreeStringArray(info.fVariantCount, info.fExtensionArray);
+
+  memset((void *)&info, 0, sizeof(info));
+
+  return NS_OK;
 }
